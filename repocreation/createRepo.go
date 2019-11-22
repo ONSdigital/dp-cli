4b55b66da970ae81c4be4e7bbb7170310b72064e
Daniel W <danieldwalford@gmail.com>
2019-11-22 12:25:43 +0000

    Added repo creation to dp-utils

--- repocreation/createRepo.go
+++ repocreation/createRepo.go
@@ -12,8 +12,18 @@ import (
 	"strings"
 )
 
+const (
+	org = "ONSdigital"
+	// Team: "DigitalPublishing", slug: "digitalpublishing", id: 779417
+	dpTeamSlug = "DigitalPublishing"
+	teamID = int64(779417)
+	defaultBranch = "develop"
+	masterBranch = "master"
+)
+
+// GenerateGithubRepository is the entry point to generating the repository
 func GenerateGithubRepository() error {
-	fmt.Println("\n\n\nThis script will create a new ONS Digital Publishing repository." +
+	fmt.Println("This script will create a new ONS Digital Publishing repository." +
 		"In order to create and configure a new repository please answer the prompts.")
 
 	accessToken, userHandle, repoName, repoDescription := getConfigurationsForNewRepo()
@@ -24,65 +34,72 @@ func GenerateGithubRepository() error {
 	tc := oauth2.NewClient(ctx, ts)
 
 	client := github.NewClient(tc)
-	org := "ONSdigital"
 
 	checkAccess(ctx, client)
-	log.Info("", log.Data{"repoDescription": repoDescription})
-	// Team: "DigitalPublishing", slug: "digitalpublishing", id: 779417
-	dpTeamSlug := "DigitalPublishing"
-	teamID := int64(779417)
 
 	repo := &github.Repository{
 		Name:          github.String(repoName),
 		Description:   github.String(repoDescription),
-		DefaultBranch: github.String("develop"),
-		MasterBranch:  github.String("master"),
+		DefaultBranch: github.String(defaultBranch),
+		MasterBranch:  github.String(masterBranch),
 		Private:       github.Bool(false),
 		HasWiki:       github.Bool(false),
 		HasProjects:   github.Bool(false),
 		AutoInit:      github.Bool(true),
 	}
 
-	err := createRepo(client, ctx, org, repo)
+	err := createRepo(client, ctx, repo)
 	if err != nil {
 		log.ErrorCtx(ctx, errors.Wrap(err, "Unable to create repository"), nil)
 		return err
 	}
 
-	err = createDevelopBranch(client, ctx, org, repoName)
+	err = createDevelopBranch(client, ctx, repoName)
 	if err != nil {
 		log.ErrorCtx(ctx, errors.Wrap(err, "Unable to create develop branch"), nil)
 		return err
 	}
 
-	err = setDevelopAsDefaultBranch(client, ctx, org, repoName, repo)
+	err = setDevelopAsDefaultBranch(client, ctx, repoName, repo)
 	if err != nil {
 		log.ErrorCtx(ctx, errors.Wrap(err, "failed to set default branch to develop"), nil)
+		return err
 	}
 
-	err = setBranchProtections(client, ctx, org, repoName, dpTeamSlug)
+	err = setBranchProtections(client, ctx, repoName)
 	if err != nil {
 		log.ErrorCtx(ctx, errors.Wrap(err, "Unable to set all branch protections, please review and correct these manually"), nil)
+		return err
 	}
 
-	setTeamsCollaborators(client, ctx, org, repoName, userHandle, teamID)
-	return err
+	err = setTeamsAndCollaborators(client, ctx, repoName, userHandle)
+	if err != nil{
+		log.ErrorCtx(ctx, errors.Wrap(err, "Unable to set team and collaborators"), nil)
+		return err
+	}
+
+	// Notify user of completion and get them to turn off actions
+	log.Info("Repository has successfully been create please Disable Actions for this repository", nil)
+	return nil
 }
 
-func setTeamsCollaborators(client *github.Client, ctx context.Context, org, repoName string, userHandle string, teamID int64) {
+// setTeamsAndCollaborators will set the DigitalPublishing team as a team working on the repo and removes the creator from being a collaborator
+func setTeamsAndCollaborators(client *github.Client, ctx context.Context, repoName string, userHandle string) error{
 	addTeamRepoOptions := github.TeamAddTeamRepoOptions{Permission: "admin"}
 	resp, err := client.Teams.AddTeamRepo(ctx, teamID, org, repoName, &addTeamRepoOptions)
 	if err != nil {
 		log.ErrorCtx(ctx, errors.Wrap(err, "Unable to add collaborators"), log.Data{"resp": resp})
 	}
 
-	client.Repositories.RemoveCollaborator(ctx, org, repoName, userHandle)
+	resp, err = client.Repositories.RemoveCollaborator(ctx, org, repoName, userHandle)
 	if err != nil {
 		log.ErrorCtx(ctx, errors.Wrap(err, "Unable to remove self as a collaborator"), log.Data{"resp": resp})
 	}
+	return err
 }
 
-func setBranchProtections(client *github.Client, ctx context.Context, org, repoName, dpTeamSlug string) error {
+// setBranchProtections sets the protections for both master and develop branches
+func setBranchProtections(client *github.Client, ctx context.Context, repoName string) error {
 	requiredStatusChecks := github.RequiredStatusChecks{
 		Strict:   true,
 		Contexts: []string{},
@@ -128,23 +145,27 @@ func setBranchProtections(client *github.Client, ctx context.Context, org, repoN
 	}
 	return err
 }
-func setDevelopAsDefaultBranch(client *github.Client, ctx context.Context, org, repoName string, repo *github.Repository) error {
+
+// setDevelopAsDefaultBranch will set the develop branch as the repositories default branch
+func setDevelopAsDefaultBranch(client *github.Client, ctx context.Context, repoName string, repo *github.Repository) error {
 	repo.AutoInit = nil
-	_,resp,err := client.Repositories.Edit(ctx,org,repoName, repo)
+	_, resp, err := client.Repositories.Edit(ctx, org, repoName, repo)
 	if err != nil {
 		log.ErrorCtx(ctx, errors.Wrap(err, "Failed to set develop as the default branch"), log.Data{"resp": resp})
 	}
 	return err
 }
 
-func createDevelopBranch(client *github.Client, ctx context.Context, org, repoName string) error {
+// createDevelopBranch will create a new branch named /develop, based on the last ref on master branch
+func createDevelopBranch(client *github.Client, ctx context.Context, repoName string) error {
 	ref, resp, err := client.Git.GetRef(ctx, org, repoName, "heads/master")
-	developBranch := "heads/develop"
-	ref.Ref = &developBranch
 	if err != nil {
 		log.ErrorCtx(ctx, errors.Wrap(err, "Get Reference to master commit failed"), log.Data{"resp": resp})
 		return err
 	}
+	developBranch := "heads/develop"
+	ref.Ref = &developBranch
+
 	_, resp, err = client.Git.CreateRef(ctx, org, repoName, ref)
 	if err != nil {
 		log.ErrorCtx(ctx, errors.Wrap(err, "Create Reference to new develop branch failed"), log.Data{"resp": resp})
@@ -153,7 +174,8 @@ func createDevelopBranch(client *github.Client, ctx context.Context, org, repoNa
 	return nil
 }
 
-func createRepo(client *github.Client, ctx context.Context, org string, repo *github.Repository) error {
+// createRepo makes a call via the gitHubAPI to generate a new repository
+func createRepo(client *github.Client, ctx context.Context, repo *github.Repository) error {
 	newRepo, _, err := client.Repositories.Create(ctx, org, repo)
 	fmt.Println(newRepo)
 	if err != nil {
@@ -163,6 +185,7 @@ func createRepo(client *github.Client, ctx context.Context, org string, repo *gi
 	return nil
 }
 
+// getConfigurationsForNewRepo gets required configuration information from the end user
 func getConfigurationsForNewRepo() (accessToken, userHandle, repoName, repoDescription string) {
 	accessToken = promptForInput("Please provide your user access token, to create one follow this guide https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line")
 	userHandle = promptForInput("Please provide your github handle/username")
@@ -171,6 +194,7 @@ func getConfigurationsForNewRepo() (accessToken, userHandle, repoName, repoDescr
 	return accessToken, userHandle, repoName, repoDescription
 }
 
+// promptForInput gives a user a message and expect input to be provided
 func promptForInput(prompt string) string {
 	fmt.Println(prompt)
 	reader := bufio.NewReader(os.Stdin)
@@ -178,6 +202,7 @@ func promptForInput(prompt string) string {
 	return strings.TrimSuffix(input, "\n")
 }
 
+// checkAccess ensures the user has provided a a valid access token
 func checkAccess(ctx context.Context, client *github.Client) {
 	_, _, err := client.Repositories.List(ctx, "", nil)
 	if err != nil {
